a59c36209c30ee06ee0e0ffb6b2c69df6b629bdb
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-02 21:11:05 +0300

    feat: загрузить из ветки main работу (проектная работа №12) спринт 14 и выставить в GitHabPage

--- app.js
+++ app.js
@@ -4,24 +4,24 @@ const bodyParser = require('body-parser');
 const routerCard = require('./routes/cards');
 const routerUser = require('./routes/users');
 
-const  PORT = 3000;
+const PORT = 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   req.user = {
-    _id: '638661605f100bfa0f871a72'
+    _id: '638661605f100bfa0f871a72',
   };
   next();
 });
 app.use('/users', routerUser);
 app.use('/cards', routerCard);
 
-mongoose.connect('mongodb://localhost:27017/mestodb',{
-  useNewUrlParser: true
-}, ()=> {
-  app.listen(PORT,()=> {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+}, () => {
+  app.listen(PORT,() => {
     console.log(`i am live, port ${PORT}`);
-  })
-})
+  });
+});
