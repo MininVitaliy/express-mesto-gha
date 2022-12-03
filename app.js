7e1f7e900e5f8568d49ab42f487266cf309beca2
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-03 17:24:07 +0300

    feat: загрузить из ветки main работу (проектная работа №12) спринт 14 и выставить в GitHabPage

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use('/users', routerUser);
 app.use('/cards', routerCard);
 app.use('*', (req, res) => {
-  return res.status(404).json({ message: 'Нет такой стараницы приложения' });
+  return res.status(404).json({ message: 'Нет такой стараницы приложения' })
 });
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
