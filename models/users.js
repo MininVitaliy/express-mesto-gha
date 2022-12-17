6c60d277a6d429145c1338e4da802c74e8463b87
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-17 21:50:55 +0300

    feat: загрузить из ветки main работу (проектная работа №13) спринт 14 и выставить в GitHabPage

--- models/users.js
+++ models/users.js
@@ -20,7 +20,7 @@ const userSchema = new mogoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (v) => validator.isURL(v),
+      validator: (v) => /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/.test(v),
       message: 'Некорректная ссылка',
     },
   },
