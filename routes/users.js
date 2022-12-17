c8c77fc20b31b4fa211e8f7cb190ff128942bac1
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-17 20:53:58 +0300

    feat: загрузить из ветки main работу (проектная работа №13) спринт 14 и выставить в GitHabPage

--- routes/users.js
+++ routes/users.js
@@ -12,10 +12,16 @@ routerUser.get('/', getUsers);
 //routerUser.post('/', createUser);
 //routerUser.get('/:userId', getUser);
 routerUser.get('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), getUser);
+/*, celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex(),
   }),
-}), getUser);
+})*/
 routerUser.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
