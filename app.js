0bc82f04a85bb76bb2b8c570382092b7c7a80456
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-18 15:40:51 +0300

    feat: загрузить из ветки main работу (проектная работа №13) спринт 14 и выставить в GitHabPage

--- app.js
+++ app.js
@@ -61,10 +61,10 @@ app.use((err, req, res, next) => {
     res.status(ERROR_CODE).json({ message: 'Переданы некорректные данные в методы создания карточки' });
   } else if (err.name === 'CastError'){
     res.status(ERROR_CODE).json({ message: 'Переданы некорректные данные iD' });
-  } else if (err.statusCode === UNAUTHORIZED) {
+  } else if (err.statusCode === 401) {
     res.status(err.statusCode).json({ message: 'Необходима авторизация' });
   } else if (err.name === 'MongoError' || err.code === 11000) {
-    res.status(CONFLICT).json({ message: 'Указанный email уже занят' });
+    res.status(409).json({ message: 'Указанный email уже занят' });
   } else {
     res.status(ERROR_SERVER).json({ message: 'Произошла ошибка' });
   }
