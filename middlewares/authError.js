87e12e34b1d802d48824263a993c7b04827666f3
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-18 17:21:42 +0300

    feat: загрузить из ветки main работу (проектная работа №13) спринт 14 и выставить в GitHabPage

--- middlewares/authError.js
+++ middlewares/authError.js
@@ -1,13 +1,12 @@
 const { UNAUTHORIZED, ERROR_CODE, CONFLICT, ERROR_SERVER } = require('../constants');
 
 function Error (err, res) {
-  console.log(err.statusCode)
   if (err.name === 'ValidationError') {
     res.status(ERROR_CODE).json({ message: 'Переданы некорректные данные в методы создания карточки' });
   } else if (err.name === 'CastError'){
     res.status(ERROR_CODE).json({ message: 'Переданы некорректные данные iD' });
   } else if (err.statusCode === 401) {
-    res.status(err.statusCode).json({ message: 'Необходима авторизация' });
+    res.status(err.statusCode).json({ message: err.message });
   } else if (err.name === 'MongoError' || err.code === 11000) {
     res.status(CONFLICT).json({ message: 'Указанный email уже занят' });
   } else {
