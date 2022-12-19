8e7c67312cd40cfe8fa970eb6fd03bc7836c61ee
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-19 22:59:43 +0300

    feat: загрузить из ветки main работу (проектная работа №13) спринт 14 и выставить в GitHabPage

--- middlewares/authError.js
+++ middlewares/authError.js
@@ -4,11 +4,13 @@ const {
   CONFLICT,
   ERROR_SERVER,
 } = require('../constants');
+const ConflictError = require('../error/ConflictError');
 
 function handlerErrors(err, req, res, next) {
   const { statusCode = 500, message } = err;
   if (err.code === 11000) {
-   return res.status(CONFLICT).json({ message: 'Указанный email уже занят' });
+   //return res.status(CONFLICT).json({ message: 'Указанный email уже занят' });
+    return next(new ConflictError('Указанный email уже занят'))
   }
   return res.status(statusCode).send({
       message: statusCode === 500
